ab5361bbfb5c469c3452cbdefa93a643395b781b
Vishal N Patel <133668085+shxdowf1ghter@users.noreply.github.com>
2023-11-10 12:16:54 -0500

    Add files via upload

---
+++ WEB303As02/WEB303As07/js/photoViewer.js
@@ -0,0 +1,13 @@
+// Name    : Vishal N Patel
+// Student : 0827195 
+
+(function ($) {
+    $.fn.photoViewer = function () {
+        
+        return this; this.click(function () {
+            alert('Image clicked!');
+        });
+
+        return this;
+    };
+})(jQuery);
